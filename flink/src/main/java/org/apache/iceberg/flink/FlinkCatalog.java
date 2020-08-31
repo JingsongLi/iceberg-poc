JingsongLi/iceberg

Flink: Integrate Flink input format to FlinkCatalog

flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.flink.table.api.TableSchema;
@@ -46,6 +47,7 @@ import org.apache.flink.table.catalog.exceptions.TableNotExistException;
 import org.apache.flink.table.catalog.stats.CatalogColumnStatistics;
 import org.apache.flink.table.catalog.stats.CatalogTableStatistics;
 import org.apache.flink.table.expressions.Expression;
+import org.apache.flink.table.factories.TableFactory;
 import org.apache.flink.util.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.CachingCatalog;
@@ -119,7 +121,7 @@ public class FlinkCatalog extends AbstractCatalog {
     return Namespace.of(namespace);
   }
 
-  private TableIdentifier toIdentifier(ObjectPath path) {
+  public TableIdentifier toIdentifier(ObjectPath path) {
     return TableIdentifier.of(toNamespace(path.getDatabaseName()), path.getObjectName());
   }
 
@@ -278,14 +280,18 @@ public class FlinkCatalog extends AbstractCatalog {
 
   @Override
   public CatalogBaseTable getTable(ObjectPath tablePath) throws TableNotExistException, CatalogException {
-    try {
-      Table table = icebergCatalog.loadTable(toIdentifier(tablePath));
-      TableSchema tableSchema = FlinkSchemaUtil.toSchema(FlinkSchemaUtil.convert(table.schema()));
+    Table table = getIcebergTable(tablePath);
+    TableSchema tableSchema = FlinkSchemaUtil.toSchema(FlinkSchemaUtil.convert(table.schema()));
 
-      // NOTE: We can not create a IcebergCatalogTable, because Flink optimizer may use CatalogTableImpl to copy a new
-      // catalog table.
-      // Let's re-loading table from Iceberg catalog when creating source/sink operators.
-      return new CatalogTableImpl(tableSchema, table.properties(), null);
+    // NOTE: We can not create a IcebergCatalogTable, because Flink optimizer may use CatalogTableImpl to copy a new
+    // catalog table.
+    // Let's re-loading table from Iceberg catalog when creating source/sink operators.
+    return new CatalogTableImpl(tableSchema, table.properties(), null);
+  }
+
+  Table getIcebergTable(ObjectPath tablePath) throws TableNotExistException {
+    try {
+      return icebergCatalog.loadTable(toIdentifier(tablePath));
     } catch (org.apache.iceberg.exceptions.NoSuchTableException e) {
       throw new TableNotExistException(getName(), tablePath, e);
     }
@@ -333,6 +339,19 @@ public class FlinkCatalog extends AbstractCatalog {
   public void alterTable(ObjectPath tablePath, CatalogBaseTable newTable, boolean ignoreIfNotExists)
       throws CatalogException {
     throw new UnsupportedOperationException("Not support alterTable now.");
+  }
+
+  CatalogLoader getCatalogLoader() {
+    return catalogLoader;
+  }
+
+  Configuration getHadoopConf() {
+    return hadoopConf;
+  }
+
+  @Override
+  public Optional<TableFactory> getTableFactory() {
+    return Optional.of(new FlinkTableFactory(this));
   }
 
   // ------------------------------ Unsupported methods ---------------------------------------------

flink/src/main/java/org/apache/iceberg/flink/FlinkTableFactory.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.ValidationException;
+import org.apache.flink.table.catalog.ObjectIdentifier;
+import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.exceptions.TableNotExistException;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.factories.TableSourceFactory;
+import org.apache.flink.table.sources.TableSource;
+import org.apache.flink.table.utils.TableSchemaUtils;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.source.FlinkTableSource;
+
+/**
+ * Flink Iceberg table factory to create table source and sink.
+ * Only works for catalog, can not be loaded from Java SPI(Service Provider Interface).
+ */
+class FlinkTableFactory implements TableSourceFactory<RowData> {
+
+  private final FlinkCatalog catalog;
+
+  FlinkTableFactory(FlinkCatalog catalog) {
+    this.catalog = catalog;
+  }
+
+  @Override
+  public Map<String, String> requiredContext() {
+    throw new UnsupportedOperationException("Iceberg Table Factory can not be loaded from Java SPI");
+  }
+
+  @Override
+  public List<String> supportedProperties() {
+    throw new UnsupportedOperationException("Iceberg Table Factory can not be loaded from Java SPI");
+  }
+
+  @Override
+  public TableSource<RowData> createTableSource(Context context) {
+    ObjectIdentifier identifier = context.getObjectIdentifier();
+    ObjectPath objectPath = new ObjectPath(identifier.getDatabaseName(), identifier.getObjectName());
+    TableIdentifier icebergIdentifier = catalog.toIdentifier(objectPath);
+    try {
+      Table table = catalog.getIcebergTable(objectPath);
+      // Excludes computed columns
+      TableSchema icebergSchema = TableSchemaUtils.getPhysicalSchema(context.getTable().getSchema());
+      return new FlinkTableSource(
+          icebergIdentifier, table, catalog.getCatalogLoader(), catalog.getHadoopConf(), icebergSchema,
+          context.getTable().getOptions());
+    } catch (TableNotExistException e) {
+      throw new ValidationException(String.format("Iceberg Table(%s) not exist.", icebergIdentifier), e);
+    }
+  }
+}

flink/src/test/java/org/apache/iceberg/flink/source/TestFlinkScanSql.java
@@ -1,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.config.TableConfigOptions;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.expressions.Expression;
+import org.apache.iceberg.flink.CatalogLoader;
+import org.apache.iceberg.flink.FlinkCatalog;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+
+/**
+ * Test Flink SELECT SQLs.
+ */
+public class TestFlinkScanSql extends TestFlinkScan {
+
+  private TableEnvironment tEnv;
+
+  public TestFlinkScanSql(String fileFormat) {
+    super(fileFormat);
+  }
+
+  @Override
+  public void before() throws IOException {
+    super.before();
+    tEnv = TableEnvironment.create(EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build());
+    tEnv.executeSql(String.format("create catalog iceberg_catalog with (" +
+                                  "'type'='iceberg', 'catalog-type'='hadoop', 'warehouse'='%s')", warehouse));
+    tEnv.executeSql("use catalog iceberg_catalog");
+    tEnv.getConfig().getConfiguration().set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
+  }
+
+  @Override
+  protected List<Row> executeWithOptions(
+      Table table, List<String> projectFields, CatalogLoader loader, Long snapshotId, Long startSnapshotId,
+      Long endSnapshotId, Long asOfTimestamp, List<Expression> filters, String sqlFilter) {
+    String fields = projectFields == null ? "*" : String.join(",", projectFields);
+
+    if (loader != null) {
+      tEnv.registerCatalog(
+          "new_catalog", new FlinkCatalog("new_catalog", "default", new String[0], loader, conf, true));
+      tEnv.executeSql("use catalog new_catalog");
+    }
+
+    Map<String, String> optionMap = Maps.newHashMap();
+    if (snapshotId != null) {
+      optionMap.put(ScanOptions.SNAPSHOT_ID.key(), snapshotId.toString());
+    }
+    if (startSnapshotId != null) {
+      optionMap.put(ScanOptions.START_SNAPSHOT_ID.key(), startSnapshotId.toString());
+    }
+    if (endSnapshotId != null) {
+      optionMap.put(ScanOptions.END_SNAPSHOT_ID.key(), endSnapshotId.toString());
+    }
+    if (asOfTimestamp != null) {
+      optionMap.put(ScanOptions.AS_OF_TIMESTAMP.key(), asOfTimestamp.toString());
+    }
+
+    StringBuilder builder = new StringBuilder();
+    optionMap.forEach((key, value) ->
+        builder.append("'").append(key).append("'").append("=").append("'").append(value).append("'").append(","));
+    String options = builder.toString();
+    if (options.endsWith(",")) {
+      options = options.substring(0, options.length() - 1);
+    }
+    if (!options.isEmpty()) {
+      options = String.format("/*+ OPTIONS(%s)*/", options);
+    }
+
+    String filter = "";
+    if (sqlFilter != null) {
+      filter = " where " + sqlFilter;
+    }
+
+    String sql = String.format("select %s from t %s %s", fields, options, filter);
+    return Lists.newArrayList(tEnv.executeSql(sql).collect());
+  }
+
+  @Override
+  protected void assertResiduals(Schema schema, List<Row> results, List<Record> writeRecords,
+                                 List<Record> filteredRecords) {
+    // should filter the data.
+    assertRecords(results, filteredRecords, schema);
+  }
+
+  @Override
+  protected void assertNestedProjection(Table table, List<Record> records) {
+    ArrayList<Row> result = Lists.newArrayList(tEnv.executeSql("select nested.f2,data from t").collect());
+
+    List<Row> expected = Lists.newArrayList();
+    for (Record record : records) {
+      expected.add(Row.of(((Record) record.get(1)).get(1), record.get(0)));
+    }
+
+    assertRows(result, expected);
+  }
+}

JingsongLi/iceberg-poc

flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
@@ -25,6 +25,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.flink.table.api.TableSchema;
@@ -48,6 +49,7 @@ import org.apache.flink.table.catalog.exceptions.TableNotExistException;
 import org.apache.flink.table.catalog.stats.CatalogColumnStatistics;
 import org.apache.flink.table.catalog.stats.CatalogTableStatistics;
 import org.apache.flink.table.expressions.Expression;
+import org.apache.flink.table.factories.TableFactory;
 import org.apache.flink.util.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.CachingCatalog;
@@ -129,7 +131,7 @@ public class FlinkCatalog extends AbstractCatalog {
     return Namespace.of(namespace);
   }
 
-  private TableIdentifier toIdentifier(ObjectPath path) {
+  public TableIdentifier toIdentifier(ObjectPath path) {
     return TableIdentifier.of(toNamespace(path.getDatabaseName()), path.getObjectName());
   }
 
@@ -292,7 +294,7 @@ public class FlinkCatalog extends AbstractCatalog {
     return toCatalogTable(table);
   }
 
-  private Table loadIcebergTable(ObjectPath tablePath) throws TableNotExistException {
+  Table loadIcebergTable(ObjectPath tablePath) throws TableNotExistException {
     try {
       return icebergCatalog.loadTable(toIdentifier(tablePath));
     } catch (org.apache.iceberg.exceptions.NoSuchTableException e) {
@@ -503,6 +505,19 @@ public class FlinkCatalog extends AbstractCatalog {
     // Let's re-loading table from Iceberg catalog when creating source/sink operators.
     // Iceberg does not have Table comment, so pass a null (Default comment value in Flink).
     return new CatalogTableImpl(schema, partitionKeys, table.properties(), null);
+  }
+
+  CatalogLoader getCatalogLoader() {
+    return catalogLoader;
+  }
+
+  Configuration getHadoopConf() {
+    return hadoopConf;
+  }
+
+  @Override
+  public Optional<TableFactory> getTableFactory() {
+    return Optional.of(new FlinkTableFactory(this));
   }
 
   // ------------------------------ Unsupported methods ---------------------------------------------

flink/src/main/java/org/apache/iceberg/flink/FlinkTableFactory.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.ValidationException;
+import org.apache.flink.table.catalog.ObjectIdentifier;
+import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.exceptions.TableNotExistException;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.factories.TableSourceFactory;
+import org.apache.flink.table.sources.TableSource;
+import org.apache.flink.table.utils.TableSchemaUtils;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.source.FlinkTableSource;
+
+/**
+ * Flink Iceberg table factory to create table source and sink.
+ * Only works for catalog, can not be loaded from Java SPI(Service Provider Interface).
+ */
+class FlinkTableFactory implements TableSourceFactory<RowData> {
+
+  private final FlinkCatalog catalog;
+
+  FlinkTableFactory(FlinkCatalog catalog) {
+    this.catalog = catalog;
+  }
+
+  @Override
+  public Map<String, String> requiredContext() {
+    throw new UnsupportedOperationException("Iceberg Table Factory can not be loaded from Java SPI");
+  }
+
+  @Override
+  public List<String> supportedProperties() {
+    throw new UnsupportedOperationException("Iceberg Table Factory can not be loaded from Java SPI");
+  }
+
+  @Override
+  public TableSource<RowData> createTableSource(Context context) {
+    ObjectIdentifier identifier = context.getObjectIdentifier();
+    ObjectPath objectPath = new ObjectPath(identifier.getDatabaseName(), identifier.getObjectName());
+    TableIdentifier icebergIdentifier = catalog.toIdentifier(objectPath);
+    try {
+      Table table = catalog.loadIcebergTable(objectPath);
+      // Excludes computed columns
+      TableSchema icebergSchema = TableSchemaUtils.getPhysicalSchema(context.getTable().getSchema());
+      return new FlinkTableSource(
+          icebergIdentifier, table, catalog.getCatalogLoader(), catalog.getHadoopConf(), icebergSchema,
+          context.getTable().getOptions());
+    } catch (TableNotExistException e) {
+      throw new ValidationException(String.format("Iceberg Table(%s) not exist.", icebergIdentifier), e);
+    }
+  }
+}

flink/src/test/java/org/apache/iceberg/flink/source/TestFlinkScanSql.java
@@ -1,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.config.TableConfigOptions;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.expressions.Expression;
+import org.apache.iceberg.flink.CatalogLoader;
+import org.apache.iceberg.flink.FlinkCatalog;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+
+/**
+ * Test Flink SELECT SQLs.
+ */
+public class TestFlinkScanSql extends TestFlinkScan {
+
+  private TableEnvironment tEnv;
+
+  public TestFlinkScanSql(String fileFormat) {
+    super(fileFormat);
+  }
+
+  @Override
+  public void before() throws IOException {
+    super.before();
+    tEnv = TableEnvironment.create(EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build());
+    tEnv.executeSql(String.format("create catalog iceberg_catalog with (" +
+                                  "'type'='iceberg', 'catalog-type'='hadoop', 'warehouse'='%s')", warehouse));
+    tEnv.executeSql("use catalog iceberg_catalog");
+    tEnv.getConfig().getConfiguration().set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
+  }
+
+  @Override
+  protected List<Row> executeWithOptions(
+      Table table, List<String> projectFields, CatalogLoader loader, Long snapshotId, Long startSnapshotId,
+      Long endSnapshotId, Long asOfTimestamp, List<Expression> filters, String sqlFilter) {
+    String fields = projectFields == null ? "*" : String.join(",", projectFields);
+
+    if (loader != null) {
+      tEnv.registerCatalog(
+          "new_catalog", new FlinkCatalog("new_catalog", "default", new String[0], loader, conf, true));
+      tEnv.executeSql("use catalog new_catalog");
+    }
+
+    Map<String, String> optionMap = Maps.newHashMap();
+    if (snapshotId != null) {
+      optionMap.put(ScanOptions.SNAPSHOT_ID.key(), snapshotId.toString());
+    }
+    if (startSnapshotId != null) {
+      optionMap.put(ScanOptions.START_SNAPSHOT_ID.key(), startSnapshotId.toString());
+    }
+    if (endSnapshotId != null) {
+      optionMap.put(ScanOptions.END_SNAPSHOT_ID.key(), endSnapshotId.toString());
+    }
+    if (asOfTimestamp != null) {
+      optionMap.put(ScanOptions.AS_OF_TIMESTAMP.key(), asOfTimestamp.toString());
+    }
+
+    StringBuilder builder = new StringBuilder();
+    optionMap.forEach((key, value) ->
+        builder.append("'").append(key).append("'").append("=").append("'").append(value).append("'").append(","));
+    String options = builder.toString();
+    if (options.endsWith(",")) {
+      options = options.substring(0, options.length() - 1);
+    }
+    if (!options.isEmpty()) {
+      options = String.format("/*+ OPTIONS(%s)*/", options);
+    }
+
+    String filter = "";
+    if (sqlFilter != null) {
+      filter = " where " + sqlFilter;
+    }
+
+    String sql = String.format("select %s from t %s %s", fields, options, filter);
+    return Lists.newArrayList(tEnv.executeSql(sql).collect());
+  }
+
+  @Override
+  protected void assertResiduals(Schema schema, List<Row> results, List<Record> writeRecords,
+                                 List<Record> filteredRecords) {
+    // should filter the data.
+    assertRecords(results, filteredRecords, schema);
+  }
+
+  @Override
+  protected void assertNestedProjection(Table table, List<Record> records) {
+    List<Row> result = Lists.newArrayList(tEnv.executeSql("select nested.f2,data from t").collect());
+
+    List<Row> expected = Lists.newArrayList();
+    for (Record record : records) {
+      expected.add(Row.of(((Record) record.get(1)).get(1), record.get(0)));
+    }
+
+    assertRows(result, expected);
+  }
+}